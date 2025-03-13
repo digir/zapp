dffe3cc86619d64711bdc608d5d452cf92e6845a
nkabindemnqobi <mnqobi.nkabinde@bbd.co.za>
2025-03-13 15:39:37 +0200

    Login (#2)
    * Added the google login now need to get the JWT without propertied
    
    * Able to get the access token from google
    
    * Verified working token by getting user details from google
    
    * Able to get google user details from aws and provide them back to the user

---
+++ project_scaffolding_server/src/main/java/com/levelUp2/project_scaffolding_server/ApiUser.java
@@ -0,0 +1,30 @@
+package com.levelUp2.project_scaffolding_server;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;;
+
+@RestController
+@RequestMapping("/api")
+public class ApiUser {
+
+    @GetMapping("/userinfo")
+    public ResponseEntity<String> getUserInfo(@RequestParam(value = "access_token") String accessToken) {
+        System.out.println("Server: Received token, verifying with Google...");
+
+        String userInfoUrl = "https://www.googleapis.com/oauth2/v3/userinfo";
+
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            String userInfo = restTemplate.getForObject(userInfoUrl + "?access_token=" + accessToken, String.class);
+            System.out.println("Server: User details retrieved from Google.");
+            return ResponseEntity.ok(userInfo);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token or request error.");
+        }
+    }
+}
---
+++ project_scaffolding_server/src/test/java/com/levelUp2/project_scaffolding_server/ProjectScaffoldingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelUp2.project_scaffolding_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectScaffoldingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
