9ad01d4b490d550efbf3813895ff63c13b149a64
Sam <samkele@bbd.co.za>
2025-03-20 11:55:14 +0200

    added template service for gemini

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -24,6 +24,7 @@ resource "aws_subnet" "public-subnet" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "af-south-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "zapp-public-subnet"
