f6579c882af48c93e1306a12b3effd66a9983f65
Jag kan HTML <vmama@ombank.co.za>
2025-03-20 09:55:59 +0200

    Remove logs & code formatting

--- cli/src/main/java/com/scaffoldcli/zapp/zapp/UserProjectConfig/ProjectStructure.java
+++ cli/src/main/java/com/scaffoldcli/zapp/zapp/UserProjectConfig/ProjectStructure.java
@@ -1,27 +1,26 @@
 package com.scaffoldcli.zapp.zapp.UserProjectConfig;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Iterator;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scaffoldcli.zapp.zapp.ServerAccess.ServerAccessHandler;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class ProjectStructure {
-    public static Map<String,String> getScaffOptions(String scaffId){
-        String scaffOptions = ServerAccessHandler.getScaffServerRequest(scaffId+"/options");
-        Map<String,String> scaffOptionNames = getScaffIdNameMap(scaffOptions);
-        return scaffOptionNames;
+    public static Map<String, String> getScaffOptions(String scaffId) {
+        String scaffOptions = ServerAccessHandler.getScaffServerRequest(scaffId + "/options");
+        return getScaffIdNameMap(scaffOptions);
     }
 
     // Fetch rendered project from API, then construct the file system
-    public static void executeFinalScaff (String scaffId){
-        String scaffToCreateJson = ServerAccessHandler.getScaffServerRequest(scaffId+"/rendered");
+    public static void executeFinalScaff(String scaffId) {
+        String scaffToCreateJson = ServerAccessHandler.getScaffServerRequest(scaffId + "/rendered");
         createFilesFromJson(scaffToCreateJson);
     }
 
@@ -29,14 +28,17 @@ public class ProjectStructure {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode rootNode = null;
 
-        try { rootNode = objectMapper.readTree(jsonString); }
-        catch (IOException e) { e.printStackTrace(); System.exit(1); return; }
+        try {
+            rootNode = objectMapper.readTree(jsonString);
+        } catch (IOException e) {
+            System.exit(1);
+            return;
+        }
 
         JsonNode filesNode = rootNode.path("files");
         processFilesNode("MyProj", filesNode);
     }
 
-    // Recursively render file system from JSON
     private static void processFilesNode(String parentDir, JsonNode filesNode) {
         Iterator<Map.Entry<String, JsonNode>> fields = filesNode.fields();
 
@@ -48,30 +50,34 @@ public class ProjectStructure {
 
             if (value.isObject()) { // Folder
                 File dir = new File(objPath);
-                if (!dir.exists()) { dir.mkdirs(); }
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
 
-                processFilesNode(objPath, value); // recurse
-            }
-            else { // File
+                processFilesNode(objPath, value);
+            } else {
                 String fileContent = value.asText();
-                try { Files.write(Paths.get(objPath), fileContent.getBytes()); }
-                catch (IOException e) { e.printStackTrace(); }
+                try {
+                    Files.write(Paths.get(objPath), fileContent.getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
-    public static Map<String, String> getScaffIdNameMap(String jsonString){
+    public static Map<String, String> getScaffIdNameMap(String jsonString) {
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, String> scaffNames = new HashMap<>();
-        
+
         try {
             JsonNode rootNode = objectMapper.readTree(jsonString);
             Iterator<Map.Entry<String, JsonNode>> fields = rootNode.fields();
-            
+
             while (fields.hasNext()) {
                 Map.Entry<String, JsonNode> field = fields.next();
                 String key = field.getKey();
-                
+
                 JsonNode value = field.getValue();
                 String name = value.get("name").asText();
                 scaffNames.put(key, name);
