0c5e90d99cafdfa736aa75c816b8b91887d76541
Jag kan HTML <vmama@ombank.co.za>
2025-03-22 12:47:36 +0200

    Add generate scaff command

--- cli/src/main/java/com/scaffoldcli/zapp/NewCLIApplication.java
+++ cli/src/main/java/com/scaffoldcli/zapp/NewCLIApplication.java
@@ -2,6 +2,7 @@ package com.scaffoldcli.zapp;
 
 import com.scaffoldcli.zapp.commands.CreateScaffCommand;
 import com.scaffoldcli.zapp.commands.HelpCommand;
+import com.scaffoldcli.zapp.commands.ScaffANewProjectCommand;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +13,7 @@ public class NewCLIApplication {
     static {
         commands.put("help", NewCLIApplication::help);
         commands.put("create", NewCLIApplication::create);
+        commands.put("scaff", NewCLIApplication::scaff);
     }
 
     public static void main(String[] args) {
@@ -31,6 +33,11 @@ public class NewCLIApplication {
 
     private static void create(String[] args) {
         CreateScaffCommand command = new CreateScaffCommand();
+        command.run(args);
+    }
+
+    private static void scaff(String[] args) {
+        ScaffANewProjectCommand command = new ScaffANewProjectCommand();
         command.run(args);
     }
 
